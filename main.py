562484414cf5924237d395688fa92426ab384ecd
Paripás Viktor <viktor.paripas@kognity.com>
2025-02-06 10:31:54 +0100

    Access payload from Slack in the endpoint

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 
 app = FastAPI()
 
 
 @app.post('/')
-async def root():
-    return {'message': 'Hello World'}
+async def root(
+    command: str = Form(...),
+    user_name: str = Form(...),
+    text: str = Form(...)
+):
+    response_message = f'@{user_name} used {command} with args: {text}'
+    return {'text': response_message}
